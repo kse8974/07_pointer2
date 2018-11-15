60459a36a81a2d6555dd25105319a6addf8d9ded
kse8974 <kse8974@naver.com>
2018-11-15 16:07:07 +0900

    calculation

--- main2.c
+++ main2.c
@@ -2,20 +2,49 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int sum(int x, int y){
+	return x+y;
+}
 
-char *proverb="All that glisters is not gold.";
+int sub(int x, int y){
+	return x-y;
+}
 
-void setPointer(char**q){
-		*q=  proverb;
-		
-			}
+int mul(int x, int y){
+	return x*y;
+}
+
+int div1(int x, int y){
+	return x/y;
+}
+
 
 int main(int argc, char *argv[]) {
 	
-	char*p="zzz";
-	setPointer(&p);
-	printf("%s\n",p);
+	int number1, number2;
+	char sign;
+	int (*calcfunc)(int, int);
 	
+	printf("숫자, 기호, 숫자를 입력하십시오        \n");
+	scanf("%d %c %d", &number1, &sign, &number2);
+
+	switch(sign){
+		case '+':
+			calcfunc = sum;
+			break;
+			
+		case '-':
+			calcfunc = sub;
+			break;
+		case '*':
+			calcfunc = mul;
+			break;
+		case '/':
+			calcfunc = div1;
+			break;
+			
+	}
+	printf("result: %i \n", calcfunc(number1, number2));
 	return 0;
-	
 }
+
